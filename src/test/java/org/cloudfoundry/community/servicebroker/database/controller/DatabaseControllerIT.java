e8e12d17c368850d6e2e2d73834c803976e85aff
Ariel Taitz <ariel.taitz@ge.com>
2018-05-22 15:45:05 +0300

    remove dependency on test order

--- src/test/java/org/cloudfoundry/community/servicebroker/database/controller/DatabaseControllerIT.java
+++ src/test/java/org/cloudfoundry/community/servicebroker/database/controller/DatabaseControllerIT.java
@@ -10,11 +10,12 @@ import org.cloudfoundry.community.servicebroker.database.Application;
 import org.cloudfoundry.community.servicebroker.database.config.CatalogConfig;
 import org.cloudfoundry.community.servicebroker.database.repository.Consts;
 import org.cloudfoundry.community.servicebroker.model.Catalog;
+import org.cloudfoundry.community.servicebroker.model.CreateServiceInstanceBindingRequest;
+import org.cloudfoundry.community.servicebroker.model.CreateServiceInstanceRequest;
 import org.cloudfoundry.community.servicebroker.model.ServiceDefinition;
-import org.junit.FixMethodOrder;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
@@ -31,49 +32,48 @@ import static com.jayway.restassured.RestAssured.given;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-/**
- * Abstract test base class for the Service Broker V2 API.
- *
- * Usage:
- * Annotate the implementing test class with the following implementation-specific annotation:
- *
- *      @SpringApplicationConfiguration(classes = Application.class)
- *
- * If you would want to test the actual creation/deletion of resources, you might also want this annotation:
- *
- *      @FixMethodOrder(MethodSorters.NAME_ASCENDING)
- *
- * This would cause JUnit to run the methods in name-ascending order, causing the cases to run in order.
- */
 @SpringApplicationConfiguration(classes = Application.class) //todo: deprecated
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles({Consts.H2})
 @WebAppConfiguration
 @IntegrationTest("server.port:0")
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DatabaseControllerIT {
 
     private static final UUID INSTANCE_ID = new UUID(1, 1);
     private static final UUID BINDING_ID = new UUID(1, 2);
-    private static final String INSTANCE_BASE_PATH = "/v2/service_instances/%s"; 
-    private static final String BINDING_BASE_PATH = "/v2/service_instances/%s/service_bindings/%s"; 
+    private static final CreateServiceInstanceBindingRequest BIND_REQUEST
+            = new CreateServiceInstanceBindingRequest("a", "b", "")
+            .withServiceInstanceId(INSTANCE_ID.toString())
+            .withBindingId(BINDING_ID.toString());
+    private static final String INSTANCE_BASE_PATH = "/v2/service_instances/%s";
+    private static final String BINDING_BASE_PATH = "/v2/service_instances/%s/service_bindings/%s";
+    private static final String PROVISION_INSTANCE_PATH;
+    private static final String CREATE_BINDING_PATH;
     private static final Header API_VERSION_HEADER = new Header("X-Broker-Api-Version", CatalogConfig.BROKER_API_VERSION); //todo: use org.cloudfoundry.community.servicebroker.model.BrokerApiVersion
 
-    private String serviceId;
-    private String planId;
+    static {
+        PROVISION_INSTANCE_PATH = String.format(INSTANCE_BASE_PATH, INSTANCE_ID);
+        CREATE_BINDING_PATH = String.format(BINDING_BASE_PATH, INSTANCE_ID, BINDING_ID);
+    }
+
+    private CreateServiceInstanceRequest createRequest;
     private Supplier<RequestSpecification> requestSupplier;
+    private String removeBindingPath;
+    private String removeInstancePath;
 
     @Autowired
     private void setupAuthentication(
             @Value("${security.user.name}") String username,
             @Value("${security.user.password}") String password) {
-        requestSupplier = () -> given().auth().basic(username, password).header(API_VERSION_HEADER); 
+        requestSupplier = () -> given().auth().basic(username, password).header(API_VERSION_HEADER);
     }
-    
+
     @Autowired
-    private void setupSpaceParameters(@Value("${space.name}") String spaceName) { //todo: why need this?
-        serviceId = "pg-" + spaceName;
-        planId = "free-" + spaceName;
+    private void setupServiceDefinition(@Value("${space.name}") String spaceName) {
+        String serviceId = "pg-" + spaceName;
+        createRequest = new CreateServiceInstanceRequest(serviceId, "a", "b", "c").withServiceInstanceId(INSTANCE_ID.toString());
+        removeInstancePath = PROVISION_INSTANCE_PATH + "?service_id=" + serviceId + "&plan_id=";
+        removeBindingPath = CREATE_BINDING_PATH + "?service_id=" + serviceId + "&plan_id=";
     }
 
     @Autowired
@@ -81,62 +81,75 @@ public class DatabaseControllerIT {
         RestAssured.port = port;
     }
 
+    @Before
+    public void cleanup() {
+        givenRequest().delete(removeBindingPath);
+        givenRequest().delete(removeInstancePath);
+    }
+
     private RequestSpecification givenRequest() {
         return requestSupplier.get();
     }
 
     @Test
-    public void case1_fetchCatalogSucceedsWithCredentials() {
-        Catalog catalog = givenRequest().get(CatalogController.BASE_PATH).then().statusCode(HttpStatus.SC_OK).extract().body().as(Catalog.class); 
-        List<ServiceDefinition> serviceDefinitions = catalog.getServiceDefinitions(); 
-        assertFalse(serviceDefinitions.isEmpty()); 
-        ServiceDefinition serviceDefinition = serviceDefinitions.iterator().next(); 
+    public void fetchCatalog() {
+        Catalog catalog = givenRequest()
+                .get(CatalogController.BASE_PATH)
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract().body().as(Catalog.class);
+
+        List<ServiceDefinition> serviceDefinitions = catalog.getServiceDefinitions();
+        assertFalse(serviceDefinitions.isEmpty());
+        ServiceDefinition serviceDefinition = serviceDefinitions.iterator().next();
         assertTrue(serviceDefinition.isBindable());
         assertFalse(serviceDefinition.getPlans().isEmpty());
     }
 
     @Test
-    public void case2_provisionInstanceSucceedsWithCredentials() {
-        String provisionInstancePath = String.format(INSTANCE_BASE_PATH, INSTANCE_ID);
-        String organizationGuid = "system";
-        String spaceGuid = "thespace";
-        String request_body = "{\n" +
-                "  \"service_id\":        \"" + serviceId + "\",\n" +
-                "  \"plan_id\":           \"" + planId + "\",\n" +
-                "  \"organization_guid\": \"" + organizationGuid + "\",\n" +
-                "  \"space_guid\":        \"" + spaceGuid + "\"\n" +
-                "}";
-
-        givenRequest().contentType(ContentType.JSON).body(request_body).put(provisionInstancePath).then().statusCode(HttpStatus.SC_CREATED);
-    }
-
-    @Test
-    public void case3_createBindingSucceedsWithCredentials() {
-        String createBindingPath = String.format(BINDING_BASE_PATH, INSTANCE_ID, BINDING_ID);
-        String request_body = "{\n" +
-                "  \"plan_id\":      \"" + planId + "\",\n" +
-                "  \"service_id\":   \"" + serviceId + "\",\n" +
-                "  \"app_guid\":     \"" + new UUID(1, 3) + "\"\n" +
-                "}";
-
+    public void provision() {
         givenRequest()
-                .contentType(ContentType.JSON).body(request_body)
-                .put(createBindingPath)
+                .contentType(ContentType.JSON)
+                .body(createRequest)
+                .put(PROVISION_INSTANCE_PATH)
                 .then()
                 .statusCode(HttpStatus.SC_CREATED)
         ;
     }
 
     @Test
-    public void case4_removeBindingSucceedsWithCredentials() {
-        String removeBindingPath = String.format(BINDING_BASE_PATH, INSTANCE_ID, BINDING_ID) + "?service_id=" + serviceId + "&plan_id=" + planId;
-        givenRequest().delete(removeBindingPath).then().statusCode(HttpStatus.SC_OK);
+    public void bind() {
+        provision();
+
+        givenRequest()
+                .contentType(ContentType.JSON)
+                .body(BIND_REQUEST)
+                .put(CREATE_BINDING_PATH)
+                .then()
+                .statusCode(HttpStatus.SC_CREATED)
+        ;
     }
 
     @Test
-    public void case5_removeInstanceSucceedsWithCredentials() {
-        String removeInstancePath = String.format(INSTANCE_BASE_PATH, INSTANCE_ID) + "?service_id=" + serviceId + "&plan_id=" + planId;
-        givenRequest().delete(removeInstancePath).then().statusCode(HttpStatus.SC_OK);
+    public void unBind() {
+        bind();
+
+        givenRequest()
+                .delete(removeBindingPath)
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+        ;
+    }
+
+    @Test
+    public void deprovision() {
+        provision();
+
+        givenRequest()
+                .delete(removeInstancePath)
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+        ;
     }
 
 }
